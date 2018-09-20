0d75ff16b0759bf2eefbd2282cc093fe543e4fb8
nikomanttari <niko.manttari@gmail.com>
2018-09-20 09:25:00 +0300

    Iftask2 version 2 done

--- IfTask2/IfTask2/Program.cs
+++ IfTask2/IfTask2/Program.cs
@@ -9,14 +9,18 @@ namespace IfTask2
             Console.WriteLine("ohjelma selvittää onko luku pariton vai parillinen");
             Console.Write("syötä numero: ");
             string userInput = Console.ReadLine();
-            int number = int.Parse(userInput);
+            int number =  0;
+            bool isNumber = int.TryParse(userInput, out number);
 
+            if (isNumber)
+            { 
             if (number % 2 == 0)
                 Console.WriteLine($"Luku {number} on parillinen");
             else
                 Console.WriteLine($"luku {number} on pariton.");
-
-             
+            }
+         else
+        Console.WriteLine("haista paska! ");
 
               
         }
