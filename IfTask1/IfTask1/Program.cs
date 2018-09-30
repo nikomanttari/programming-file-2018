e56ff236a8295e7e80c51e011d61e9f09c31abdf
nikomanttari <niko.manttari@gmail.com>
2018-09-30 20:25:39 +0300

    iftask 1 done

---
+++ IfTask1/IfTask1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfTask1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko luku negatiivinen, positiivinen vai nolla");
+            Console.Write("Syötä numero ");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+            if (isNumber)
+            {
+                if (number == 0)
+                    Console.WriteLine($"Numero {number} on nolla");
+                else if (number < 0)
+                    Console.WriteLine($"Numero {number} on negatiivinen");
+                else
+                    Console.WriteLine($"Numero {number} on positiivinen");
+               
+
+
+            }
+            else
+                Console.WriteLine("väärä syöte");
+        }
+    }
+}
