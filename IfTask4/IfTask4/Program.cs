519de308f799c3ea5b55292e8526b72ac7368791
nikomanttari <niko.manttari@gmail.com>
2018-10-02 16:59:13 +0300

    IfTask4 done

---
+++ IfTask4/IfTask4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IfTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma lajittelee numerot nousevaan järjestykseen");
+            Console.Write("Syötä 1. numero: ");
+            string userInput = Console.ReadLine();
+            int a = int.Parse(userInput);
+            Console.Write("Syötä 2. numero: ");
+            userInput = Console.ReadLine();
+            int b = int.Parse(userInput);
+            Console.Write("Syötä 3. numero: ");
+            userInput = Console.ReadLine();
+            int c = int.Parse(userInput);
+            
+            if (a > c)
+            {
+                if (b > c)
+                    Console.Write($"{c}, {b} ja {a} ");
+                else
+                {
+                    if (c > a)
+                        Console.Write($"{b}, {a} ja {c} ");
+                    else
+                        Console.Write($"{b}, {c} ja {a} ");
+                }
+            }
+            else
+            {
+                if (a > c)
+                    Console.Write($"{c}, {a} ja {b} ");
+                else
+                {
+                    if (b > c)
+                        Console.Write($"{a}, {c} ja {b} ");
+                    else
+                        Console.Write($"{a}, {b} ja {c} ");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ TicketPrice/TicketPrice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicketPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää maatalousnäyttelyn lipun hinnan");
+            int ticketPrice = 16;
+            double discount = 0.15; //15% ale MTK
+
+
+
+            Console.WriteLine($"Lipun hinta on {ticketPrice * discount}");
+        }
+    }
+}
