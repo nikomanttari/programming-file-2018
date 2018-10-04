3f030752b59fb58d92044953fecca18c62710ae0
nikomanttari <niko.manttari@gmail.com>
2018-10-04 10:56:07 +0300

    TicketPrice done

--- TicketPrice/TicketPrice/Program.cs
+++ TicketPrice/TicketPrice/Program.cs
@@ -8,11 +8,50 @@ namespace TicketPrice
         {
             Console.WriteLine("Ohjelma selvittää maatalousnäyttelyn lipun hinnan");
             int ticketPrice = 16;
-            double discount = 0.15; //15% ale MTK
+            double discount = 0 ; //15% ale MTK
+            int number = 0;
+            
+            Console.WriteLine("Oletko Varusmies K vai E");
+            string userInput = Console.ReadLine();
 
+            if (userInput == "K")
+                discount = 0.50;
+            else
+            {
+                Console.WriteLine("Oletko opiskelija K tai E");
+                userInput = Console.ReadLine();
+                if (userInput == "K")
+                {
+                    Console.WriteLine("Oletko MTK jäsen K tai E?");
+                    userInput = Console.ReadLine();
 
+                    if (userInput == "K")
+                        discount = 0.60;
+                    else
+                        discount = 0.45;
+                }
+                else
+                {
+                    Console.WriteLine("Syötä ikä");
+                    string userInput1 = Console.ReadLine();
+                    number = int.Parse(userInput1);
 
-            Console.WriteLine($"Lipun hinta on {ticketPrice * discount}");
+                    if (number < 7)
+                        discount = 1;
+                    else if (number >= 7 && number <= 15 || number >= 65)
+                        discount = 0.5;
+                    else
+                    {
+                        Console.WriteLine("Oletko MTK jäsen K tai E?");
+                        userInput = Console.ReadLine();
+
+                        if (userInput == "K")
+                            discount = 0.15;
+                    }
+
+                }
+            }
+            Console.WriteLine($"Lipun hinta on {ticketPrice-ticketPrice * discount}");
         }
     }
 }
