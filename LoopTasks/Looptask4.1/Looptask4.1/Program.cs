8adaf5b644ea27a7284c013e1bab895779162efb
nikomanttari <niko.manttari@gmail.com>
2018-11-12 09:50:00 +0200

    looptask4.1 done

---
+++ LoopTasks/Looptask4.1/Looptask4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Looptask4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret.");
+
+            {
+                
+
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    Console.WriteLine($"{i+1})  {Math.Sqrt(i+1)}");
+                }
+            }
+        }
+    }
+}
