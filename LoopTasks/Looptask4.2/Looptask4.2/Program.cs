5d8811e517fa828bb7ec6d63134d073c9defc277
nikomanttari <niko.manttari@gmail.com>
2018-11-12 10:14:34 +0200

    looptask4.2 done

---
+++ LoopTasks/Looptask4.2/Looptask4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Looptask4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9.");
+            Console.WriteLine("Syötä luku");
+            int number = int.Parse(Console.ReadLine());
+            
+                for (int i = 1; i < 10; i++)
+
+                {
+                    Console.WriteLine($"   {number} * {i} = {number *i} ");
+                }
+                        
+            
+
+        }
+    }
+}
