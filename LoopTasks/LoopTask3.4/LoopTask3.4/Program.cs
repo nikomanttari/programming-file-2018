90c075b3ab0d30f379c3ade7e57abb4d4f5999c1
nikomanttari <niko.manttari@gmail.com>
2018-11-12 09:00:14 +0200

    looptask 3.4 done

---
+++ LoopTasks/LoopTask3.4/LoopTask3.4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LoopTask3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee ensimmäistä lukua yhteen positiivisilla ja negatiivisilla numeroilla");
+
+            int number = 0;
+            int summa = 0;
+            do
+            {
+                Console.WriteLine("Syötä luku");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                //number = int.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+
+            }
+            
+
+            while (number == 0);
+            if (number > 0)
+
+
+            {
+                for (int i = 1; i <= number; i++)
+
+
+                {
+                    summa += i;
+                }
+               
+
+            }
+
+            else
+            {
+                for (int i = -1; i >= number; i--)
+                {
+                    summa += i;
+                }
+               
+
+            }
+
+            Console.WriteLine($"Luvun {number} summa on {summa}");
+
+
+        }
+    }
+}
